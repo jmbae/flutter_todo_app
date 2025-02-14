0f99457a20ea6fb4217d2a34e1377759f7c8212a
JM Bae <jungman82@gmail.com>
2025-02-14 17:03:30 +0900

    shared_preferences 패키지 추가 및 설정 서비스에서 로케일 저장 기능 구현

--- lib/src/settings/settings_service.dart
+++ lib/src/settings/settings_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsService {
   Future<ThemeMode> themeMode() async => ThemeMode.system;
@@ -8,10 +9,16 @@ class SettingsService {
     // http package to persist settings over the network.
   }
 
-  Future<Locale> locale() async => Locale('en');
+  Future<Locale> locale() async {
+    final prefs = await SharedPreferences.getInstance();
+    final locale = prefs.getString('locale');
+    return Locale(locale ?? 'en');
+  }
 
   Future<void> updateLocale(Locale locale) async {
     // Use the shared_preferences package to persist settings locally or the
     // http package to persist settings over the network.
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('locale', locale.languageCode);
   }
 }
