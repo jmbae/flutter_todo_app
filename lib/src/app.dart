45a347afdcd2efccbabd5abf0b3057509f470802
JM Bae <jungman82@gmail.com>
2025-02-14 16:22:51 +0900

    메인 화면에 할 일 목록 뷰 추가 및 라우팅 설정 변경

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,6 +5,8 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
 
+import 'todo_list_view.dart';
+
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
   const MyApp({
@@ -42,8 +44,10 @@ class MyApp extends StatelessWidget {
                 switch (routeSettings.name) {
                   case SettingsView.routeName:
                     return SettingsView(controller: settingsController);
+                  case TodoListView.routeName:
+                    return const TodoListView();
                   default:
-                    return const Placeholder();
+                    return const TodoListView();
                 }
               },
             );
---
+++ lib/src/todo_list_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'settings/settings_view.dart';
+
+class TodoListView extends StatefulWidget {
+  const TodoListView({super.key});
+  static const routeName = '/';
+
+  @override
+  State<TodoListView> createState() => _TodoListViewState();
+}
+
+class _TodoListViewState extends State<TodoListView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Todo App'), actions: [
+        IconButton(
+            onPressed: () {
+              Navigator.restorablePushNamed(context, SettingsView.routeName);
+            },
+            icon: const Icon(Icons.settings))
+      ]),
+    );
+  }
+}
